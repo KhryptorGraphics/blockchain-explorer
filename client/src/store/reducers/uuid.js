c7a8c835706d040cae9002bc3087c5a37ebc725b
Akarsh Agarwal <akarsh.agrawal2010@gmail.com>
2018-06-04 06:32:30 -0400

    Little bit formatting.

--- client/src/store/reducers/uuid.js
+++ client/src/store/reducers/uuid.js
@@ -1,6 +1,7 @@
 import { handleActions } from 'redux-actions'
 import { Record } from 'immutable'
 import * as actionTypes from '../actions/action-types'
+import moment from 'moment-timezone'
 
 const InitialState = new Record({
     loaded: false,
@@ -9,10 +10,14 @@ const InitialState = new Record({
 });
 
 const uuid = handleActions({
-    [actionTypes.UUID_ROW]: (state = InitialState(), action) => state
-        .set('loaded', true)
-        .set('uuid', action.payload)
-        .set('errors', action.error)
+    [actionTypes.UUID_ROW]: (state = InitialState(), action) => {
+        return (
+            state
+                .set('loaded', true)
+                .set('uuid', action.payload)
+                .set('errors', action.error)
+        )
+    }
 }, new InitialState());
 
 export default uuid;
